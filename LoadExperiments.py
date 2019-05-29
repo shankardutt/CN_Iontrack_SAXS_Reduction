7ef104baf55109a451bd98f00c657951882543f9
Christian Notthoff <christian.notthoff@anu.edu.au>
2019-05-29 13:09:28 +1000

    pyFAI poni read routine, error in energy/wavelenght corrected

--- LoadExperiments.py
+++ LoadExperiments.py
@@ -52,9 +52,10 @@ class scatterBrainRead:
                 if(child.tag == "CONFIGURATION"):
                     if(child.get('NAME') == 'SAXS'):
                         camera_defs=child.find("CAMERADEFS")
+
                         self.dictionary["dist"]=float(camera_defs.get('LENGTH'))*1e-3
                         self.dictionary["Beam_x"]=float(camera_defs.get('BEAMX'))
-                        self.dictionary["Beam_y"]=float(camera_defs.get('BEAMY'))
+                        self.dictionary["Beam_y"]=float(1679)-float(camera_defs.get('BEAMY'))
 #                        self.dictionary['energy_wavelength_box']=0
 #                        self.dictionary['energy_wavelength']=12.3984/float(camera_defs.get('WAVELENGTH'))
                         self.dictionary["energy"]=12.3984/float(camera_defs.get('WAVELENGTH'))
@@ -70,8 +71,9 @@ class pyFAIRead:
         self.dictionary["dist"]=float(poniFile.dist)
         self.dictionary["Beam_x"]=float(poniFile.poni2)/float(dictionary["pixel2"])
         self.dictionary["Beam_y"]=float(poniFile.poni1)/float(dictionary["pixel1"])
-        self.dictionary["energy"]=12.3984/float(poniFile.wavelength)
-
+        self.dictionary["energy"]=12.3984/(float(poniFile.wavelength)*1e10)
+        print(float(poniFile.wavelength))
+        print(self.dictionary["energy"])
      def get_experiment(self):
         return self.dictionary
 
