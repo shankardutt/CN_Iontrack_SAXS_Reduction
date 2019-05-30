0b8f43a8f4e5e76130a7ef951099e8e1eaf3f359
Christian Notthoff <christian.notthoff@anu.edu.au>
2019-05-30 10:12:19 +1000

    removed some debug prints

--- LoadExperiments.py
+++ LoadExperiments.py
@@ -72,8 +72,7 @@ class pyFAIRead:
         self.dictionary["Beam_x"]=float(poniFile.poni2)/float(dictionary["pixel2"])
         self.dictionary["Beam_y"]=float(poniFile.poni1)/float(dictionary["pixel1"])
         self.dictionary["energy"]=12.3984/(float(poniFile.wavelength)*1e10)
-        print(float(poniFile.wavelength))
-        print(self.dictionary["energy"])
+
      def get_experiment(self):
         return self.dictionary
 
